7da22c7bcffcb1cefdadadcc5e6fc984948c94b9
luckydonald <mac2019.git@luckydonald.de>
2020-01-05 21:32:54 +0100

    First WIP for `sendChatAction`.

--- views/api/v4_4/sendable.py
+++ views/api/v4_4/sendable.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from enum import Enum
 from tools import r_success
 from typing import Union
 from fastapi import APIRouter as Blueprint, HTTPException
@@ -52,4 +53,65 @@ async def send_message(
     )
     data = await to_web_api(msg, bot)
     return r_success(data.to_array())
+# end def
+
+
+class ChatAction(Enum):
+    TYPING = "typing"
+    UPLOAD_PHOTO = "upload_photo"
+    RECORD_VIDEO = "record_video"
+    UPLOAD_VIDEO = "upload_video"
+    RECORD_AUDIO = "record_audio"
+    UPLOAD_AUDIO = "upload_audio"
+    UPLOAD_DOCUMENT = "upload_document"
+    FIND_LOCATION = "find_location"
+    RECORD_VIDEO_NOTE = "record_video_note"
+    UPLOAD_VIDEO_NOTE = "upload_video_note"
+
+    # extra:
+    PLAY_GAME = "play_game"
+    CHOOSE_CONTACT = "choose_contact"
+    CANCEL = "cancel"
+# end class
+
+
+actions_api_to_telethon_mapping = {
+    ChatAction.TYPING: "typing",
+    ChatAction.UPLOAD_PHOTO: "photo",
+    ChatAction.RECORD_VIDEO: "record-video",
+    ChatAction.UPLOAD_VIDEO: "video",
+    ChatAction.RECORD_AUDIO: "record-audio",
+    ChatAction.UPLOAD_AUDIO: "audio",
+    ChatAction.UPLOAD_DOCUMENT: "document",
+    ChatAction.FIND_LOCATION: "location",
+    ChatAction.RECORD_VIDEO_NOTE: "record-round",
+    ChatAction.UPLOAD_VIDEO_NOTE: "round",
+    # extra:
+    ChatAction.CANCEL: "cancel",
+    ChatAction.PLAY_GAME: "game",
+    ChatAction.CHOOSE_CONTACT: "contact",
+}
+
+
+@routes.api_route('/{token}/sendChatAction', methods=["GET", "POST"], tags=['send'])
+async def send_chat_action(
+    token: str = TOKEN_VALIDATION,
+    chat_id: Union[int, str] = Query(..., description="Unique identifier for the target chat or username of the target channel (in the format @channelusername)", regex=r"@[a-zA-Z][a-zA-Z0-9_]{2,}"),
+    action: ChatAction = Query(..., description='Type of action to broadcast. Choose one, depending on what the user is about to receive: "typing" for text messages, "upload_photo" for photos, "record_video" or "upload_video" for "videos", "record_audio" or "upload_audio" for audio files, "upload_document" for general files, "find_location" for location data, "record_video_note" or "upload_video_note" for video notes. Additionally added by this API implementation are "play_game", "choose_contact" and "cancel".'),
+):
+    from main import _get_bot
+    bot = await _get_bot(token)
+    try:
+        entity = await get_entity(bot, chat_id)
+        # end try
+    except ValueError:
+        raise HTTPException(404, detail="chat not found?")
+    # end try
+    from telethon.client.chats import ChatMethods
+
+    action: str = actions_api_to_telethon_mapping[action]
+    await bot.action(entity, action=action)
+
+    from tools import r_success
+    return r_success()
 # end def
