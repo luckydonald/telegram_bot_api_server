5c5b7fbf528ae8824b6b50928762f4dd9f3bc8a7
luckydonald <mac2019.git@luckydonald.de>
2020-01-19 15:35:18 +0100

    Implemented `sendVenue`.

--- telegram_bot_api_server/views/api/v4_5/sendable/location.py
+++ telegram_bot_api_server/views/api/v4_5/sendable/location.py
@@ -10,6 +10,7 @@ from telethon.tl.types import (
     InputGeoPoint as TInputGeoPoint,
     InputMediaGeoLive as TInputMediaGeoLive,
     InputGeoPointEmpty as TInputGeoPointEmpty,
+    InputMediaVenue as TInputMediaVenue,
 )
 
 __author__ = 'luckydonald'
@@ -204,6 +205,71 @@ async def stop_message_live_location(
         message=message_id,
         buttons=buttons,
     )
+    data = await to_web_api(result, bot)
+    return r_success(data.to_array())
+# end def
+
+
+@routes.api_route('/{token}/sendVenue', methods=['GET', 'POST'], tags=['official', 'send', 'location'])
+async def send_venue(
+    token: str = TOKEN_VALIDATION,
+    chat_id: Union[int, str] = Query(..., description='Unique identifier for the target chat or username of the target channel (in the format @channelusername)'),
+    latitude: float = Query(..., description='Latitude of the venue'),
+    longitude: float = Query(..., description='Longitude of the venue'),
+    title: str = Query(..., description='Name of the venue'),
+    address: str = Query(..., description='Address of the venue'),
+    foursquare_id: Optional[str] = Query(None, description='Foursquare identifier of the venue'),
+    foursquare_type: Optional[str] = Query(None, description='Foursquare type of the venue, if known. (For example, "arts_entertainment/default", "arts_entertainment/aquarium" or "food/icecream".)'),
+    disable_notification: Optional[bool] = Query(None, description='Sends the message silently. Users will receive a notification with no sound.'),
+    reply_to_message_id: Optional[int] = Query(None, description='If the message is a reply, ID of the original message'),
+    reply_markup: Optional[Json[Union['InlineKeyboardMarkupModel', 'ReplyKeyboardMarkupModel', 'ReplyKeyboardRemoveModel', 'ForceReplyModel']]] = Query(None, description='Additional interface options. A JSON-serialized object for an inline keyboard, custom reply keyboard, instructions to remove reply keyboard or to force a reply from the user.'),
+) -> JSONableResponse:
+    """
+    Use this method to send information about a venue. On success, the sent Message is returned.
+
+    https://core.telegram.org/bots/api#sendvenue
+    """
+    reply_markup: Optional[InlineKeyboardMarkupModel] = parse_obj_as(
+        Optional[InlineKeyboardMarkupModel],
+        obj=reply_markup,
+    )
+
+    # models -> bot
+    buttons = await to_telethon(reply_markup, None)
+
+    from .....main import _get_bot
+    bot = await _get_bot(token)
+
+    try:
+        entity = await get_entity(bot, chat_id)
+    except BotMethodInvalidError:
+        assert isinstance(chat_id, int) or (isinstance(chat_id, str) and len(chat_id) > 0 and chat_id[0] == '@')
+        entity = chat_id
+    except ValueError:
+        raise HTTPException(404, detail="chat not found?")
+    # end try
+
+    file = TInputMediaVenue(
+        geo_point=TInputGeoPoint(
+            lat=latitude,
+            long=longitude,
+        ),
+        title=title,
+        address=address,
+        provider='foursquare' if foursquare_id or foursquare_type else '',
+        venue_id=foursquare_id if foursquare_id else '',
+        venue_type=foursquare_type if foursquare_type else '',
+    )
+
+    # noinspection PyTypeChecker
+    result = await bot.send_file(
+        entity=entity,
+        file=file,
+        silent=disable_notification,
+        reply_to=reply_to_message_id,
+        buttons=buttons,
+    )
+
     data = await to_web_api(result, bot)
     return r_success(data.to_array())
 # end def
