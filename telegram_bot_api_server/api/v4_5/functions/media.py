ec4ae94734b20e87541080c43d7b1f2cdc855b0b
luckydonald <mac2019.git@luckydonald.de>
2020-01-24 09:37:38 +0100

    Having a function for processing input files.

--- telegram_bot_api_server/api/v4_5/functions/media.py
+++ telegram_bot_api_server/api/v4_5/functions/media.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from enum import Enum
+import httpx
 from io import BytesIO
 from os import path, SEEK_END, SEEK_SET
-from tempfile import SpooledTemporaryFile, TemporaryFile
+from enum import Enum
 from typing import Union, Optional, List
-
-import httpx
-from fastapi import APIRouter as Blueprint, HTTPException, UploadFile
+from fastapi import APIRouter as Blueprint, HTTPException, UploadFile as FastApiUploadFile
+from tempfile import SpooledTemporaryFile, TemporaryFile
 from fastapi.params import Query, Param, File
 from pydantic.fields import FieldInfo
-from starlette.concurrency import run_in_threadpool
-from starlette.requests import Request
 from telethon.errors import BotMethodInvalidError
+from starlette.requests import Request
+from starlette.datastructures import UploadFile as StarletteUploadFile
+from starlette.concurrency import run_in_threadpool
 from telethon.tl.types import TypeSendMessageAction, InputStickerSetShortName, InputMessageID
 from telethon.client.chats import _ChatAction
 from luckydonaldUtils.files.mime import get_file_suffix
@@ -39,54 +39,15 @@ if __name__ == '__main__':
 routes = Blueprint()  # Basically a Blueprint
 
 
-@routes.api_route('/{token}/sendPhoto', methods=['GET', 'POST'], tags=['official'])
-async def send_photo(
-    token: str = TOKEN_VALIDATION,
-    chat_id: Union[int, str] = Query(..., description='Unique identifier for the target chat or username of the target channel (in the format @channelusername)'),
-    photo: 'InputFileModel' = File(..., description='Photo to send. Pass a file_id as String to send a photo that exists on the Telegram servers (recommended), pass an HTTP URL as a String for Telegram to get a photo from the Internet, or upload a new photo using multipart/form-data. More info on Sending Files »'),
-    caption: Optional[str] = Query(None, description='Photo caption (may also be used when resending photos by file_id), 0-1024 characters'),
-    parse_mode: Optional[str] = Query(None, description='Send Markdown or HTML, if you want Telegram apps to show bold, italic, fixed-width text or inline URLs in the media caption.'),
-    disable_notification: Optional[bool] = Query(None, description='Sends the message silently. Users will receive a notification with no sound.'),
-    reply_to_message_id: Optional[int] = Query(None, description='If the message is a reply, ID of the original message'),
-    reply_markup: Optional[Json[Union['InlineKeyboardMarkupModel', 'ReplyKeyboardMarkupModel', 'ReplyKeyboardRemoveModel', 'ForceReplyModel']]] = Query(None, description='Additional interface options. A JSON-serialized object for an inline keyboard, custom reply keyboard, instructions to remove reply keyboard or to force a reply from the user.'),
-    request: Request=None,
-) -> JSONableResponse:
-    """
-    Use this method to send photos. On success, the sent Message is returned.
-
-    https://core.telegram.org/bots/api#sendphoto
-    """
-    file = photo
-    file: Union[InputFileModel, str] = parse_obj_as(
-        Union[InputFileModel, str],
-        obj=file,
-    )
-    reply_markup: Optional[Union[InlineKeyboardMarkupModel, ReplyKeyboardMarkupModel, ReplyKeyboardRemoveModel, ForceReplyModel]] = parse_obj_as(
-        Optional[Union[InlineKeyboardMarkupModel, ReplyKeyboardMarkupModel, ReplyKeyboardRemoveModel, ForceReplyModel]],
-        obj=reply_markup,
-    )
-
-    from ....main import _get_bot
-    bot = await _get_bot(token)
-
-    try:
-        entity = await get_entity(bot, chat_id)
-    except BotMethodInvalidError:
-        assert isinstance(chat_id, int) or (isinstance(chat_id, str) and len(chat_id) > 0 and chat_id[0] == '@')
-        entity = chat_id
-    except ValueError:
-        raise HTTPException(404, detail="chat not found?")
-    # end try
-
-    given_file = file
+async def process_file(given_file: Union[str, FastApiUploadFile, StarletteUploadFile], request: Request) -> Union[BytesIO, str, JSONableResponse]:
     if isinstance(given_file, str):
         # url, attachment or file_id
         # noinspection PyProtectedMember
         if given_file.startswith('attach://'):
-            field = given_file[9:]
+            field_name = given_file[9:]
             # new we need to qurey the resulting field
             form = await request.form()
-            if not form[field]:
+            if not form[field_name]:
                 from pydantic import MissingError
                 from fastapi.exceptions import RequestValidationError
                 from pydantic.error_wrappers import ErrorWrapper
@@ -94,15 +55,14 @@ async def send_photo(
                 raise RequestValidationError(
                     errors=[
                         ErrorWrapper(
-                            MissingError(), loc=('form', field)
+                            MissingError(), loc=('form', field_name)
                          ),
                     ],
                 )
             # end if
-            # noinspection PyTypeChecker
-            file: UploadFile = form[field]
+
             try:
-                UploadFile.validate(file)
+                FastApiUploadFile.validate(given_file)
             except ValueError as e:
                 from pydantic import MissingError
                 from fastapi.exceptions import RequestValidationError
@@ -111,36 +71,14 @@ async def send_photo(
                 raise RequestValidationError(
                     errors=[
                         ErrorWrapper(
-                            e, loc=('form', field)
+                            e, loc=('form', field_name)
                         ),
                     ],
                 )
             # end try
-            file_name = file.filename
-            original_file: UploadFile = file
-            underlying_file: SpooledTemporaryFile = original_file.file
-            # underlying_file is a file-like object, and has .seek and .tell available.
-            # determine the size by jumping to the end and reading the position
-            await run_in_threadpool(underlying_file.seek, 0, SEEK_END)
-            file_size = await run_in_threadpool(underlying_file.tell)
-            # reset to position 0 to be able to read it again later.
-            await run_in_threadpool(underlying_file.seek, 0, SEEK_SET)
-            # check that size
-            if file_size > DOWNLOAD_MAX_SIZE:
-                return r_error(
-                    400,
-                    description="BAD REQUEST: failed to get HTTP URL content (File size too large)",
-                    result={'reason': 'size', 'size': file_size},
-                )
-            # end if
-
-            real_file: Union[BytesIO, TemporaryFile] = underlying_file._file
-            if isinstance(file, BytesIO):
-                file: BytesIO = real_file
-            else:
-                file: BytesIO = BytesIO(real_file.read())
-            # end if
-            file.name = file_name
+            # noinspection PyTypeChecker
+            file: FastApiUploadFile = form[field_name]
+            return await process_file(file, request)
         elif given_file.startswith('http'):  # either http:// and https://
             from urllib.parse import urlparse
             parsed_url = urlparse(given_file)
@@ -186,7 +124,7 @@ async def send_photo(
                             # end if
                         # end for
                         downloaded_file.seek(0)
-                        file: BytesIO = downloaded_file
+                        return downloaded_file
                     # end with
                 except httpx.exceptions.HTTPError as e:
                     return r_error(
@@ -197,9 +135,84 @@ async def send_photo(
                 # end try
             # end with
         else:
-            file: str = given_file
+            # probably a file_id
+            return given_file
         # end if
+    elif isinstance(given_file, (FastApiUploadFile, StarletteUploadFile)):
+        file_name = given_file.filename
+        underlying_file: SpooledTemporaryFile = given_file.file
+        # underlying_file is a file-like object, and has .seek and .tell available.
+        # determine the size by jumping to the end and reading the position
+        await run_in_threadpool(underlying_file.seek, 0, SEEK_END)
+        file_size = await run_in_threadpool(underlying_file.tell)
+        # reset to position 0 to be able to read it again later.
+        await run_in_threadpool(underlying_file.seek, 0, SEEK_SET)
+        # check that size
+        if file_size > DOWNLOAD_MAX_SIZE:
+            return r_error(
+                400,
+                description="BAD REQUEST: failed to get HTTP URL content (File size too large)",
+                result={'reason': 'size', 'size': file_size},
+            )
+        # end if
+
+        real_file: Union[BytesIO, TemporaryFile] = underlying_file._file
+        if isinstance(real_file, BytesIO):
+            file: BytesIO = real_file
+        else:
+            file: BytesIO = BytesIO(real_file.read())
+        # end if
+        file.name = file_name
+        return file
     # end if
+    return None
+# end def
+
+
+@routes.api_route('/{token}/sendPhoto', methods=['GET', 'POST'], tags=['official'])
+async def send_photo(
+    token: str = TOKEN_VALIDATION,
+    chat_id: Union[int, str] = Query(..., description='Unique identifier for the target chat or username of the target channel (in the format @channelusername)'),
+    photo: 'InputFileModel' = File(..., description='Photo to send. Pass a file_id as String to send a photo that exists on the Telegram servers (recommended), pass an HTTP URL as a String for Telegram to get a photo from the Internet, or upload a new photo using multipart/form-data. More info on Sending Files »'),
+    caption: Optional[str] = Query(None, description='Photo caption (may also be used when resending photos by file_id), 0-1024 characters'),
+    parse_mode: Optional[str] = Query(None, description='Send Markdown or HTML, if you want Telegram apps to show bold, italic, fixed-width text or inline URLs in the media caption.'),
+    disable_notification: Optional[bool] = Query(None, description='Sends the message silently. Users will receive a notification with no sound.'),
+    reply_to_message_id: Optional[int] = Query(None, description='If the message is a reply, ID of the original message'),
+    reply_markup: Optional[Json[Union['InlineKeyboardMarkupModel', 'ReplyKeyboardMarkupModel', 'ReplyKeyboardRemoveModel', 'ForceReplyModel']]] = Query(None, description='Additional interface options. A JSON-serialized object for an inline keyboard, custom reply keyboard, instructions to remove reply keyboard or to force a reply from the user.'),
+    request: Request=None,
+) -> JSONableResponse:
+    """
+    Use this method to send photos. On success, the sent Message is returned.
+
+    https://core.telegram.org/bots/api#sendphoto
+    """
+    file = photo
+    file: Union[InputFileModel, str] = parse_obj_as(
+        Union[InputFileModel, str],
+        obj=file,
+    )
+    reply_markup: Optional[Union[InlineKeyboardMarkupModel, ReplyKeyboardMarkupModel, ReplyKeyboardRemoveModel, ForceReplyModel]] = parse_obj_as(
+        Optional[Union[InlineKeyboardMarkupModel, ReplyKeyboardMarkupModel, ReplyKeyboardRemoveModel, ForceReplyModel]],
+        obj=reply_markup,
+    )
+
+    from ....main import _get_bot
+    bot = await _get_bot(token)
+
+    try:
+        entity = await get_entity(bot, chat_id)
+    except BotMethodInvalidError:
+        assert isinstance(chat_id, int) or (isinstance(chat_id, str) and len(chat_id) > 0 and chat_id[0] == '@')
+        entity = chat_id
+    except ValueError:
+        raise HTTPException(404, detail="chat not found?")
+    # end try
+
+    file: Union[BytesIO, str] = await process_file(given_file=photo, request=request)
+    if isinstance(file, JSONableResponse):
+        # abort
+        return file
+    # end def
 
     buttons = await to_telethon(reply_markup, bot)
 
