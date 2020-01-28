9e95f50a6f2ff90bc96381456918a32dabc08b56
luckydonald <mac2019.git@luckydonald.de>
2020-01-28 18:34:36 +0100

    Parse TG_APP_ID as number.

--- telegram_bot_api_server/environment_vars.py
+++ telegram_bot_api_server/environment_vars.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
 
 TG_APP_ID = os.getenv('TG_APP_ID', None)
 assert TG_APP_ID is not None  # $TG_APP_ID environment variable
+TG_APP_ID = int(TG_APP_ID)  # parse $TG_APP_ID as integer.
 
 TG_APP_HASH = os.getenv('TG_APP_HASH', None)
 assert TG_APP_HASH is not None  # $TG_APP_HASH environment variable#
