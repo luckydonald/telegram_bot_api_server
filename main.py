03ab291957ed19bde98cf8ca4b66e0156f210488
luckydonald <mac2019.git@luckydonald.de>
2020-01-29 15:36:36 +0100

    Running a remote debugger, to avoid the script output slowing down the IDE so much.

--- main.py
+++ main.py
@@ -2,6 +2,13 @@
 # -*- coding: utf-8 -*-
 from telegram_bot_api_server.main import app, main
 from luckydonaldUtils.logger import logging
+try:
+    # noinspection PyUnresolvedReferences
+    import pydevd_pycharm
+    pydevd_pycharm.settrace('localhost', port=56221, suspend=False)
+except (ImportError, ConnectionRefusedError):
+    pass
+# end try
 
 __author__ = 'luckydonald'
 
